5dd04891394c4dd493dbee626e45dfa7cb3c1aec
zachary webb <zachsavn@gmail.com>
2022-04-05 10:42:30 -0400

    completed exercise

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate"
+const GIPHY_KEY = "TIh4sY44qZD8fQp7h89BUwROphX4BVKM"
+let inputElement = document.querySelector("#gifSearch")
+let searchBtnElement = document.querySelector("#search")
+let imgEle = document.querySelector("#giphy")
+
+searchBtnElement.addEventLestener("click", searchGif)
+
+function searchGif() {
+    let searchTerm = inputEle.value
+
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`)
+    .then((result) => result.json)
+    .then((contents)=> {
+        imgEle.src = contents.data.images.original.url;
+    })
+    .catch((err)=> console.error(err));
+}
