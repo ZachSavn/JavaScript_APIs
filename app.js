98254ed6ad127f6129c35177ce0e41d9daa14332
zachary webb <zachsavn@gmail.com>
2022-04-05 20:07:13 -0400

    debugged

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate"
-const GIPHY_KEY = "TIh4sY44qZD8fQp7h89BUwROphX4BVKM"
-let inputElement = document.querySelector("#gifSearch")
-let searchBtnElement = document.querySelector("#search")
-let imgElement = document.querySelector("#giphy")
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "TIh4sY44qZD8fQp7h89BUwROphX4BVKM";
 
-searchBtnElement.addEventListener("click", searchGif)
+let inputElement = document.querySelector("#gifSearch");
+let searchBtnElement = document.querySelector("#search");
+let imgElement = document.querySelector("#giphy");
+
+searchBtnElement.addEventListener("click", searchGif);
 
 function searchGif() {
-    let searchTerm = inputElement.value
+  let searchTerm = inputElement.value;
 
-    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`)
-    .then((result) => result.json())//call not reference when just .json
-    .then((contents)=> {
-        imgElement.src = contents.data.images.original.url;
+  fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`)
+    .then((result) => result.json()) //call not reference when just .json
+    .then((contents) => {
+      imgElement.src = contents.data.images.original.url;
     })
-    .catch((err)=> console.error(err));
+    .catch((err) => console.error(err));
 }
